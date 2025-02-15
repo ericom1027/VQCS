95cd847c6e09260b6d5dd946822ea2438af5ad7f
ericsondalay <eicsondalay@gmail.com>
2025-02-15 23:20:33 +0800

    modify admin dashboard

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ app.get("*", (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 // Export for external use
